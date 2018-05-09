a20743aa8b1e8d3801b5229885a238c26fd67c7f
Alberto Mansilla <mansilla_alberto@outlook.com>
2018-05-08 23:29:29 -0300

    Segundo intento de corregir las ID

---
+++ Tp1/Migrations/20180509022855_KeysChangedV2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Tp1.Migrations
+{
+    public partial class KeysChangedV2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
